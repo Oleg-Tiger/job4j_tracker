a047b78d5dd74ccef461046f69c1a6c26ba34d8e
Oleg-Tiger <free_person@inbox.ru>
2021-03-12 11:57:43 +0300

    4. Переопределение [#480954]

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -4,8 +4,8 @@ public class JSONReport extends TextReport {
 
     @Override
     public String generate(String name, String body) {
-       String one = String.format("\"name\" : \"%s\",", name);
-       String two = String.format("\"body\" : \"%s\"", body);
+       String one = "{" + System.lineSeparator() + String.format("\"name\" : \"%s\",", name);
+       String two = String.format("\"body\" : \"%s\"", body) + System.lineSeparator() + "}";
         return one + System.lineSeparator() + two;
     }
 }
