6d2e22a8887aa310bfabf5d907bcc547f51d78d9
Oleg-Tiger <free_person@inbox.ru>
2021-03-17 16:29:33 +0300

    3. Интерфейс Input [#480892]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int paxCount) {
+
+    }
+
+    @Override
+    public double fillUp(double litresCount) {
+        return 0;
+    }
+}
