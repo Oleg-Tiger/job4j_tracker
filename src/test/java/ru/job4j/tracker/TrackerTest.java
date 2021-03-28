8f67b845da98bc31ed5afbe0bbb15b2a1c9fa126
Oleg-Tiger <free_person@inbox.ru>
2021-03-29 01:30:20 +0300

    0. Сортировка [#480867]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.core.IsNull.nullValue;
@@ -39,6 +45,59 @@ public class TrackerTest {
         int id = bug.getId();
         tracker.delete(id);
         assertThat(tracker.findById(id), is(nullValue()));
+    }
+
+    @Test
+    public void WhenSortedByIdMinToMax() {
+        List<Item> list = new ArrayList<>();
+        Item item1 = new Item("name1", 333);
+        Item item2 = new Item("name2", 3);
+        Item item3 = new Item("name3", 33);
+        list.add(item3);
+        list.add(item1);
+        list.add(item2);
+        Collections.sort(list, new SortByIdItemMinToMax());
+        assertThat(list, is(Arrays.asList(item2, item3, item1)));
+    }
+
+    @Test
+    public void WhenSortedByIdMaxToMin() {
+        List<Item> list = new ArrayList<>();
+        Item item1 = new Item("name1", 333);
+        Item item2 = new Item("name2", 3);
+        Item item3 = new Item("name3", 33);
+        list.add(item3);
+        list.add(item1);
+        list.add(item2);
+        Collections.sort(list, new SortByIdItemMaxToMin());
+        assertThat(list, is(Arrays.asList(item1, item3, item2)));
+    }
+
+    @Test
+    public void WhenSortedByNameMinToMax() {
+        List<Item> list = new ArrayList<>();
+        Item item1 = new Item("name1", 333);
+        Item item2 = new Item("name2", 3);
+        Item item3 = new Item("name3", 33);
+        list.add(item3);
+        list.add(item1);
+        list.add(item2);
+        Collections.sort(list, new SortByNameItemMinToMax());
+        assertThat(list, is(Arrays.asList(item1, item2, item3)));
+    }
+
+    @Test
+    public void WhenSortedByNameMaxToMin() {
+        List<Item> list = new ArrayList<>();
+        Item item1 = new Item("name1", 333);
+        Item item2 = new Item("name2", 3);
+        Item item3 = new Item("name3", 33);
+        list.add(item3);
+        list.add(item1);
+        list.add(item2);
+        Collections.sort(list, new SortByNameItemMaxToMin());
+        assertThat(list, is(Arrays.asList(item3, item2, item1)));
+
     }
 
 }
