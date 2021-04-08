718fa9cdd1e46f9f5c453901c0780590abd55fce
Oleg-Tiger <free_person@inbox.ru>
2021-04-08 14:21:57 +0300

    2. Список адресов [#480703]

--- src/main/java/ru/job4j/stream/Profile.java
+++ src/main/java/ru/job4j/stream/Profile.java
@@ -3,9 +3,15 @@ package ru.job4j.stream;
 import java.util.Objects;
 
 public class Profile {
+    private String name;
     private Address address;
 
     public Profile(Address address) {
+        this.address = address;
+    }
+
+    public Profile(String name, Address address) {
+        this.name = name;
         this.address = address;
     }
 
@@ -17,4 +23,29 @@ public class Profile {
         this.address = address;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Profile profile = (Profile) o;
+        return Objects.equals(name, profile.name)
+                && Objects.equals(address, profile.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address);
+    }
 }
