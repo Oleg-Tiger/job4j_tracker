cce75a861267f8a02c8e585a36afdcbd6cb78623
MasterPC <free_person@inbox.ru>
2022-11-29 19:27:26 +0300

    2. ManyToMany [#331986]

---
+++ db/scripts/005_ddl_create_participates_table.sql
@@ -0,0 +1,5 @@
+CREATE table if not exists participates (
+   id serial PRIMARY KEY,
+   item_id int not null REFERENCES items(id),
+   user_id int not null REFERENCES j_user(id)
+);
--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -2,10 +2,12 @@ package ru.job4j.tracker;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ru.job4j.toone.User;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +22,14 @@ public class Item {
     private int id;
     private String name;
     private LocalDateTime created = LocalDateTime.now();
+
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "item_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id") }
+    )
+    private List<User> participates;
 
     public Item(String name) {
         this.name = name;
